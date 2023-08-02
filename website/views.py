e1c4b4bc9ed32e15add6de78592d323499186ac6
Beckham <guilhermebeckham14@hotmail.com>
2023-08-02 17:03:34 -0300

    feat: :sparkles: Creating the login and logout routes and functions

--- website/views.py
+++ website/views.py
@@ -1,4 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
 
 def home(request):
-    return render(request, 'home.html', {})
+    # Check to see if the user is logged in
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        #Authenticating the user
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "User logged in")
+            return redirect('home')
+        else:
+            messages.error(request, "There was a problem authenticating the user, please try again...")
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "User logged out")
+    return redirect('home')
